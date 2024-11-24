ce841dbfb8c1d74fef567f39d7d972ca68e07265
Phil Glew-Deval <philglew@yahoo.co.uk>
2024-11-24 14:23:01 +0000

    ui

--- NovaHR/novahr.client/src/App.jsx
+++ NovaHR/novahr.client/src/App.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const [showLoginBox, setShowLoginBox] = useState(false);
-
-    const handleCloseLoginBox = () => {
-        setShowLoginBox(false);
-    };
-
     return (
         <div className="app">
             <header className="toolbar">
                 <nav className="navbar">
                     <ul>
                         <li><a href="#">NovaHR</a></li>
-                        <li><a href="#!" onClick={(e) => { e.preventDefault(); setShowLoginBox(true); }}>Login</a></li>
+                        <li><a href="#">Login</a></li>
                         <li><a href="#">About NovaHR</a></li>
                         <li><a href="#">Plans and pricing</a></li>
                         <li><a href="#">Sign up</a></li>
@@ -27,16 +21,6 @@ function App() {
                     <p>Your all-in-one solution for HR management</p>
                 </div>
             </main>
-            {showLoginBox && (
-                <div className="login-box">
-                    <h2>Login</h2>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <button>Login</button>
-                    <button style={{ marginTop: '10px' }}>Login with Entra ID</button>
-                    <button onClick={handleCloseLoginBox} style={{ marginTop: '10px', backgroundColor: '#ccc', color: 'black' }}>Close</button>
-                </div>
-            )}
         </div>
     );
 }
