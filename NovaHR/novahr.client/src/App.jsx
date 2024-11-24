da8efe95c8dd367ebaa5db87ca02c70b4cf4dff1
Phil Glew-Deval <philglew@yahoo.co.uk>
2024-11-24 14:10:22 +0000

    ui

--- NovaHR/novahr.client/src/App.jsx
+++ NovaHR/novahr.client/src/App.jsx
@@ -1,35 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const [showLogin, setShowLogin] = useState(false);
-
-    const handleLoginClick = () => {
-        setShowLogin(true);
-    };
-
     return (
         <div className="app">
-            <div className="toolbar">
+            <header className="toolbar">
                 <nav className="navbar">
                     <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="#login" onClick={handleLoginClick}>Login</a></li>
+                        <li><a href="#">NovaHR</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">About NovaHR</a></li>
+                        <li><a href="#">Plans and pricing</a></li>
+                        <li><a href="#">Sign up</a></li>
                     </ul>
                 </nav>
-            </div>
-            {showLogin && (
-                <div className="login-box">
-                    <h1>Login</h1>
-                    <form>
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit">Submit</button>
-                    </form>
+            </header>
+            <main className="main-content">
+                <div className="welcome-box">
+                    <h1>Welcome to NovaHR</h1>
+                    <p>Your all-in-one solution for HR management</p>
                 </div>
-            )}
+            </main>
         </div>
     );
 }
